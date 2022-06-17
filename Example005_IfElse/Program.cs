5a1d2158a6aada22b8b7fa5876ad641702c3ff00
artem <artem.titov.wg@gmail.com>
2022-06-17 17:57:33 +0300

    Example 005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "аня")
+{
+    Console.WriteLine("Ура, это же Аня!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
